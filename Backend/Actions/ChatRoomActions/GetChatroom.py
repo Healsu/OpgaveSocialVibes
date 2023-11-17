74a218f1c4cea52f9b246b376764a018559044b5
Nicolas-Mousten <nicolas.mousten@gmail.com>
2023-11-17 13:59:51 +0100

    Changed from firestore to realtime database and refactored all the code to use the realtime database syntax instead

--- Backend/Actions/ChatRoomActions/GetChatroom.py
+++ Backend/Actions/ChatRoomActions/GetChatroom.py
@@ -11,7 +11,7 @@ def getDataAndMessages(chatroom_id):
     chatroom_data = chatroom_ref.get()
     chatroom_participants_data = chatroom_participants_ref.get()
     try:
-        if chatroom_messages_ref.get().exists():
+        if chatroom_messages_ref.get() is not None:
             chatroom_messages_data = chatroom_messages_ref.get()
             return {"Chatroom":chatroom_data, "Participants":chatroom_participants_data, "Messages":chatroom_messages_data}
     except:
