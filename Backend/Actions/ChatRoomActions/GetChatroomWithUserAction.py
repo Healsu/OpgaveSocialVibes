b98a9ff716c5b6c4c7f6b57c242e73ff1c990295
Nicolas-Mousten <nicolas.mousten@gmail.com>
2023-12-07 12:36:47 +0100

    adh

--- Backend/Actions/ChatRoomActions/GetChatroomWithUserAction.py
+++ Backend/Actions/ChatRoomActions/GetChatroomWithUserAction.py
@@ -4,7 +4,7 @@ def userInChatroom(user_id):
     try:
         db = FireBaseDatabase.getConnection()
         chatrooms = db.child("Chatroom Participants").get().items()
-        chatroom_objects = []
+        chatroom_objects = {}
         for chatroom in chatrooms:#sort the chatrooms
             for profile in chatroom[1]:#sort out profiles from the chatroom
                 if(profile["id"] == user_id):#sort for user id in the chatroom
@@ -16,7 +16,7 @@ def userInChatroom(user_id):
                         'Type': chatroom_data.get("Type", None)
                     }
 
-                    chatroom_objects.append({f"{chatroom[0]}": f"{data}"})#append the chatroom to the return array
+                    chatroom_objects[chatroom[0]] = data #append the chatroom to the return array
         FireBaseDatabase.closeConnection()
         return chatroom_objects
     except:
