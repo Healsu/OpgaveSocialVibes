c03fb2f01894331bb0b01d6823078271f1683349
Nicolas-Mousten <nicolas.mousten@gmail.com>
2023-12-13 12:18:23 +0100

    sorted

--- Backend/Actions/CommunityActions/GetCommunityWhitoutUserAction.py
+++ Backend/Actions/CommunityActions/GetCommunityWhitoutUserAction.py
@@ -1,10 +1,23 @@
-from Database import FireBaseDatabase
+from Database import FireBaseDatabase, Querys
 
 
 def get(user_id):
     db = FireBaseDatabase.getReferenceFromConnection("Chatrooms")
-
+    
     # Query to get all chatrooms where the type is equal to 'Community'
     query_result = db.order_by_child("Type").equal_to("Community").get()
+    
+    FireBaseDatabase.closeConnection()
+    
 
-    return query_result
+    db = FireBaseDatabase.getConnection()
+    chatroom_keys = {}
+    user_object = Querys.getById(db, "Profiles", user_id)
+    
+    for key, value in query_result.items():
+        participants_list = db.child("Chatroom Participants").child(key).get()
+
+        if( user_object in participants_list):
+            chatroom_keys[key] = value
+    
+    return chatroom_keys
